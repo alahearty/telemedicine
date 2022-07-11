54ccaba4f0582bc42b1eaf65984e7ed710a5ad15
Wori Hearty Alapher <alapher.woriayibapri@cyphercrescent.com>
2022-07-11 16:34:47 +0100

    Updates

--- telemedicine-webapi/src/Domain/Entities/TelemedicineService.cs
+++ telemedicine-webapi/src/Domain/Entities/TelemedicineService.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace telemedicine_webapi.Domain.Entities;
 public class TelemedicineService : BaseAuditableEntity
 {
-    public string? ServiceName { get; set; }
-    public double Amount { get; set; }
-    public string? Description { get; set; }
+    public virtual string? ServiceName { get; set; }
+    public virtual double Amount { get; set; }
+    public virtual string? Description { get; set; }
 }
