4bb6a3cce54f3fc5c20c53c13d1fe4947a2cf936
Yahya-Mana Abubakar <yahya-mana.abubakar@svr.cyphercrescent.com>
2022-07-22 08:33:06 +0100

    Landing page footer

--- telemedicine-spa/src/components/home-landing/Footer.vue
+++ telemedicine-spa/src/components/home-landing/Footer.vue
@@ -1,6 +1,8 @@
 <template>
   <footer>
     <section class="newsletter">
+      <img class="img-bg" src="@/assets/img/footer-bg.png" />
+
       <div class="inner">
         <div>
           <h2>Get an update every week</h2>
@@ -20,17 +22,51 @@
       </div>
     </section>
     <section class="footer-links">
-      <div class="inner"></div>
+      <img class="img-bg" src="@/assets/img/footer-dot-bg.png" />
+      <div class="inner">
+        <div class="promo-container">
+          <TelemedicineLogo />
+          <p>
+            The world's most trusted <br />
+            telehealth company.
+          </p>
+        </div>
+        <div class="links-container">
+          <ul>
+            <li class="link-title">Departments</li>
+            <li>Eye care</li>
+            <li>Cardiac care</li>
+            <li>Heart care</li>
+          </ul>
+          <ul>
+            <li class="link-title">Membership</li>
+            <li>Free trial</li>
+            <li>Silver</li>
+            <li>Premium</li>
+          </ul>
+          <ul>
+            <li class="link-title">Customer care</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Get update</li>
+          </ul>
+        </div>
+      </div>
     </section>
     <section class="copyright">
-      <div class="inner"></div>
+      <div class="inner">
+        <p>All rights reserved &copy; <span>Telemedicine</span> 2022</p>
+      </div>
     </section>
   </footer>
 </template>
 
 <script>
+import TelemedicineLogo from '@/components/icons/TelemedicineHomeFooterLogo.vue'
+
 export default {
   name: 'Footer',
+  components: { TelemedicineLogo },
 }
 </script>
 
@@ -49,15 +85,29 @@ footer section .inner {
   width: 100%;
 }
 .newsletter,
+.newsletter {
+  position: relative;
+}
+
+.newsletter .img-bg {
+  position: absolute;
+  top: -8.125rem;
+  right: 0;
+  width: 25%;
+}
+
+.newsletter,
 .footer-links {
   padding-block: 7rem;
 }
+
 .newsletter .inner {
   display: flex;
   gap: 1.5rem;
 }
 .newsletter .inner > div,
 .newsletter .inner > form {
+  z-index: 1;
   flex: 0.5;
 }
 .newsletter h2 {
@@ -100,10 +150,63 @@ footer section .inner {
 }
 
 .footer-links {
+  position: relative;
   background-color: rgba(27, 113, 161, 0.75);
 }
+.footer-links .inner {
+  display: flex;
+  justify-content: space-between;
+}
 
-@media (max-width: 1024px) {
+.footer-links .img-bg {
+  position: absolute;
+  top: -3.25rem;
+  left: 0;
+  opacity: 0.2;
+}
+
+.footer-links svg {
+  color: #fff;
+  height: 50px;
+  width: 250px;
+}
+
+.footer-links .promo-container p {
+  margin-top: 1.5rem;
+  font-weight: 600;
+}
+
+.footer-links .links-container {
+  flex: 0.6;
+  display: flex;
+  justify-content: space-between;
+}
+.footer-links ul .link-title {
+  margin-bottom: 2rem;
+  font-size: 1.2rem;
+  font-weight: 700;
+  color: #f0f4f9;
+  cursor: default;
+}
+
+.footer-links li {
+  margin-bottom: 0.5rem;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.copyright {
+  padding-block: 2rem;
+  text-align: center;
+  color: #dbdbe9;
+  font-weight: 700;
+  font-size: 0.875rem;
+}
+.copyright span {
+  color: #0dafca;
+}
+
+@media (max-width: 900px) {
   .newsletter .inner {
     flex-direction: column;
     gap: 2.5rem;
