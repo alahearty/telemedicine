b38797b867aefd2446b8b97489ae54bde4465228
Wori Hearty Alapher <alapher.woriayibapri@cyphercrescent.com>
2022-07-25 16:37:13 +0100

    Sample Update

--- telemedicine-webapi/src/Infrastructure/ConfigureServices.cs
+++ telemedicine-webapi/src/Infrastructure/ConfigureServices.cs
@@ -21,11 +21,12 @@ public static class ConfigureServices
     {
         services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
 
+        var sqlserverConnection = new StaticFiles();
         var jwtSettings = new JwtSettings();
         configuration.GetSection("JwtSettings").Bind(jwtSettings);
         services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
         services.AddScoped<AuditableEntitySaveChangesInterceptor>();
-        configuration.GetSection(StaticFiles.SettingName).Bind(SqlServerConnectionSetting);
+        configuration.GetSection(StaticFiles.SettingName).Bind(sqlserverConnection);
         if (configuration.GetValue<bool>("UseInMemoryDatabase"))
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("telemedicine_webapiDb"));
